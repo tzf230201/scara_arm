72ba0403ae63a8b42aaff48cb6685be65262a89d
zikri <teukuzikrifatahillah@gmail.com>
2025-04-29 21:05:20 +0700

    after testing adjustment

--- pusirobot/ver_b/b5_gui.py
+++ pusirobot/ver_b/b5_gui.py
@@ -155,16 +155,16 @@ def homing():
     
     last_time = time.time()
 
-def routine():
-    if is_already_wake_up():
-        read_present_position()
-        # servo_get_motor_velocity(0x601)
-        # servo_get_status_word(0x601)
-        # Memanggil fungsi print_continuously lagi setelah 1000 ms (1 detik)
-        delta_time = time.time() - last_time
-        print(f"time : {delta_time:.2f}")
+# def routine():
+#     if is_already_wake_up():
+#         read_present_position()
+#         # servo_get_motor_velocity(0x601)
+#         # servo_get_status_word(0x601)
+#         # Memanggil fungsi print_continuously lagi setelah 1000 ms (1 detik)
+#         delta_time = time.time() - last_time
+#         print(f"time : {delta_time:.2f}")
     
-    root.after(500, routine)
+#     root.after(500, routine)
     
 # Menangani sinyal SIGINT (Ctrl + C)
 signal.signal(signal.SIGINT, lambda signum, frame: signal_handler())
