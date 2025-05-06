400b2e8bfe80ad9d3dd814f695fbe634edd5584c
zikri <teukuzikrifatahillah@gmail.com>
2025-05-06 19:35:21 +0700

    after testing adjustment

--- pusirobot/ver_b/b4_function.py
+++ pusirobot/ver_b/b4_function.py
@@ -39,11 +39,11 @@ def read_present_position():
     # formatted_angles = "°, ".join([f"{angle:.2f}" for angle in cur_joints]) #6 may 2025
     # print_yellow(f"cur joint : {formatted_angles}°")#yellow #6 may 2025
     j1, j2, j3, j4 = cur_joints #6 may 2025
-    print_yellow(f"cur joint : j1:{j1:.1f}°")#yellow #6 may 2025
+    print_yellow(f"cur servo's angle: {j1:.1f}°")#yellow #6 may 2025
     
     cur_x, cur_y, cur_z, cur_yaw = cur_coor
     # print_orange(f"cur coor : x:{cur_x:.1f} mm, y:{cur_y:.1f} mm, z:{cur_z:.1f} mm, yaw:{cur_yaw:.1f}°")#orange #6 may 2025
-    print_orange(f"cur coor : z:{cur_z:.1f} mm")#orange #6 may 2025
+    print_orange(f"cur coor z:{cur_z:.1f} mm")#orange #6 may 2025
     
     servo_vel = servo_get_motor_velocity(ID1)
     servo_status = servo_get_status_word(ID1)
