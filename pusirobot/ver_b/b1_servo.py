c7ae930a217e62cdbc1518020ba0b9aa6caff9ad
zikri <teukuzikrifatahillah@gmail.com>
2025-05-06 20:02:26 +0700

    after testing adjustment

--- pusirobot/ver_b/b1_servo.py
+++ pusirobot/ver_b/b1_servo.py
@@ -154,8 +154,12 @@ def servo_init():
     _,val = req_nmt(0x601)
     print(f"val: {val:02X}")
     if val == 0x7F:
-        print(f"servo is not operational")
+        print(f"servo is not operational, going to operational mode")
         servo_goto_operational()
+        time.sleep(1)
+        print(f"ok, servo in oeprational mode")
+    elif val == 0x05:
+        print(f"servo is already in operational mode")
     servo_switch_on()
     servo_set_operation_mode(1)
     # servo_read_operation_mode()
