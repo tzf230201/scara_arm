e933ad9942e8ea3173f9d0fc03e8b908fb89d32f
tzf230201 <tzf230201@gmail.com>
2025-05-07 10:01:33 +0700

    after testing adjustment

--- pusirobot/ver_b/b5_servo_test.py
+++ pusirobot/ver_b/b5_servo_test.py
@@ -5,7 +5,7 @@ from b4_function import wake_up, shutdown, read_present_position, get_encoder_po
 from b3_motion import dancing,dancing2, sp_angle, sp_coor, pvt_circular, pvt_mode_try_pvt_3, pp_angle, pp_coor
 from b1_servo import servo_get_motor_velocity, servo_get_status_word
 import sys
-
+#ege
 # class Tee:
 #     def __init__(self, file_name):
 #         self.console = sys.stdout  # Standard output (console)
