1246bed901eb08a752b57b4d39ec999d9100677d
tzf230201 <tzf230201@gmail.com>
2025-06-16 15:10:22 +0700

    after testing adjustment

--- pusirobot/ver_b/b4_function.py
+++ pusirobot/ver_b/b4_function.py
@@ -6,7 +6,7 @@ is_wake_up = False
 def wake_up():
     global is_wake_up
     start_can()
-    # stepper_init() #6 may 2025
+    stepper_init() #6 may 2025
     is_wake_up = True
     servo_init()
 
@@ -15,7 +15,7 @@ def is_already_wake_up():
     return is_wake_up
 
 def shutdown():
-    # stepper_shutdown() #6 may 2025
+    stepper_shutdown() #6 may 2025
     servo_shutdown()
     print(f"servo shutdown")
     # stop_can()
@@ -36,14 +36,14 @@ def read_present_position():
     cur_joints = get_cur_joints()
     cur_coor = forward_kinematics(cur_joints)
     
-    # formatted_angles = "°, ".join([f"{angle:.2f}" for angle in cur_joints]) #6 may 2025
-    # print_yellow(f"cur joint : {formatted_angles}°")#yellow #6 may 2025
-    j1, j2, j3, j4 = cur_joints #6 may 2025
-    print_yellow(f"cur servo's angle: {j1:.1f}°")#yellow #6 may 2025
+    formatted_angles = "°, ".join([f"{angle:.2f}" for angle in cur_joints]) #6 may 2025
+    print_yellow(f"cur joint : {formatted_angles}°")#yellow #6 may 2025
+    # j1, j2, j3, j4 = cur_joints #6 may 2025
+    # print_yellow(f"cur servo's angle: {j1:.1f}°")#yellow #6 may 2025
     
     cur_x, cur_y, cur_z, cur_yaw = cur_coor
-    # print_orange(f"cur coor : x:{cur_x:.1f} mm, y:{cur_y:.1f} mm, z:{cur_z:.1f} mm, yaw:{cur_yaw:.1f}°")#orange #6 may 2025
-    print_orange(f"cur coor z:{cur_z:.1f} mm")#orange #6 may 2025
+    print_orange(f"cur coor : x:{cur_x:.1f} mm, y:{cur_y:.1f} mm, z:{cur_z:.1f} mm, yaw:{cur_yaw:.1f}°")#orange #6 may 2025
+    # print_orange(f"cur coor z:{cur_z:.1f} mm")#orange #6 may 2025
     
     servo_vel = servo_get_motor_velocity(ID1)
     servo_status = servo_get_status_word(ID1)
