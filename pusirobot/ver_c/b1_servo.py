ef3e3b0c991c80664ec20a78c69fa319c1aef70a
tzf230201 <tzf230201@gmail.com>
2025-06-23 00:14:30 +0700

    use newer servo, building new code

--- pusirobot/ver_c/b1_servo.py
+++ pusirobot/ver_c/b1_servo.py
@@ -137,7 +137,7 @@ def decode_opeation_mode(operation_mode):
 
 def servo_read_operation_mode():
     operation_mode = req_sdo(ID1, OD_SERVO_MODE_OF_OPERATION, 0x00)
-    print(f"Operation mode: {operation_mode:02X}")
+    # print(f"Operation mode: {operation_mode:02X}")
     decode_opeation_mode(operation_mode)
 
 def servo_shutdown():
