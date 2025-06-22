27639bd74c5e833b45dd99096323fb9462f8185c
tzf230201 <tzf230201@gmail.com>
2025-06-22 19:57:26 +0700

    use newer servo, building new code

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
-git status
-git add .
-git status
-git commit -m "use newer servo, building new code" *
-git push origin main
+# Cek apakah username adalah bukan 'peter'
+if [ "$(whoami)" != "peter" ]; then
+    git status
+    git add .
+    git status
+    git commit -m "use newer servo, building new code" *
+    git push origin main
+else
+    echo "pengguna 'peter', tidak dapat menjalankan script ini."
+fi
