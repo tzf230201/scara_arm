01c1aa060aa17e545ad9c3e95c4bc334e02feb3b
tzf230201 <tzf230201@gmail.com>
2025-06-24 11:12:44 +0700

    use newer servo, building new code

--- pusirobot/ver_c/b1_servo.py
+++ pusirobot/ver_c/b1_servo.py
@@ -115,8 +115,8 @@ def servo_accel_decel_calc(d_total, t_travel_ms):
     accel_pps2 = (2 * d_accel) / (t_accel ** 2)   # pulse/s²
     v_max_pps = accel_pps2 * t_accel              # pulse/s
 
-    accel_scaled = (int)(abs(accel_pps2 * 10))         # → 0.1 count/s²
-    v_max_scaled = (int)(abs(v_max_pps * 10))         # → 0.1 count/s
+    accel_scaled = (int)(abs(accel_pps2))         # → 0.1 count/s²
+    v_max_scaled = (int)(abs(v_max_pps))         # → 0.1 count/s
     print(f"accel_scaled: {accel_scaled}, v_max_scaled: {v_max_scaled}")
 
     return accel_scaled, v_max_scaled
