f164f64897ddc3da233959ad54c8f0c512378600
tzf230201 <tzf230201@gmail.com>
2025-06-24 10:22:01 +0700

    use newer servo, building new code

--- pusirobot/ver_c/b3_motion.py
+++ pusirobot/ver_c/b3_motion.py
@@ -621,3 +621,6 @@ def from_jmc_homing():
     servo_set_max_speed(0x0A)
     servo_set_tar_pulse(0x00)
     set_sdo(ID1, SET_2_BYTE, OD_SERVO_CONTROL_WORD, 0x00,  0x1F)
+    
+# apakah bisa set origin untuk position mode?
+# berapa count per revolusi?
