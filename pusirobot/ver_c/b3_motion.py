6445384599bc9af7e6786dee8c632a723167c84e
tzf230201 <tzf230201@gmail.com>
2025-07-03 23:07:30 +0700

    use newer servo, building new code

--- pusirobot/ver_c/b3_motion.py
+++ pusirobot/ver_c/b3_motion.py
@@ -216,7 +216,7 @@ def pp_angle(tar_joints, travel_time, max_speed, selection):
         
     if selection != "stepper_only":  
         time.sleep(0.5)  # wait for servo to switch on
-        set_sdo(ID1, SET_2_BYTE, OD_SERVO_CONTROL_WORD, 0x00,  0x3F)
+        set_sdo(ID1, SET_2_BYTE, OD_SERVO_CONTROL_WORD, 0x00,  0x1F)
     
 def pp_coor(tar_coor, travel_time, max_speed, selection):
     tar_joints = inverse_kinematics(tar_coor)
