2228f195f8ee3f2a678b41a366f6dd8499e00a44
tzf230201 <tzf230201@gmail.com>
2025-07-07 09:51:46 +0700

    use newer servo, building new code

--- pusirobot/ver_c/b3_motion.py
+++ pusirobot/ver_c/b3_motion.py
@@ -3,6 +3,7 @@ import math
 from b1_servo import *
 from b1_stepper import *
 from b2_pvt import *
+import numpy as np
 # enc: 131744993 -794543, -802111, -802962
 #enc: 131876599 -794544, -802109, -802962
 
