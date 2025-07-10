730d46cfd74f5742ab812eca191c8a78e9585e7f
tzf230201 <tzf230201@gmail.com>
2025-07-11 01:18:32 +0700

    new pvt testing

--- pusirobot/ver_c/b5_gui.py
+++ pusirobot/ver_c/b5_gui.py
@@ -272,7 +272,8 @@ def routine():
             
             
             read_present_position()
-            print_red(f"{motion_type}, {travel_time} ms, d1: {d1}, d2: {d2}, d3: {d3}, d4: {d4}")
+            # print_red(f"{motion_type}, {travel_time} ms, d1: {d1}, d2: {d2}, d3: {d3}, d4: {d4}")
+            print_red(f"tar coor, x: {d1} mm, y: {d2} mm, z: {d3} mm, yaw: {d4}°")
             motion_cnt += 1
             # print(f"counter {motion_cnt} of {motion_size}")
             execute_motion_data(entry)
