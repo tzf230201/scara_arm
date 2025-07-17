52a76c4631ff456f6efc1c6a3a60cdabfb8c4c01
tzf230201 <tzf230201@gmail.com>
2025-07-18 00:04:05 +0700

    new pvt testing

--- pusirobot/ver_d/b3_motion.py
+++ pusirobot/ver_d/b3_motion.py
@@ -40,10 +40,10 @@ def load_origin_from_config():
     print(f"{config_path}")
     
     default_config = {
-        "origin_1": 0,
-        "origin_2": 0,
-        "origin_3": 0,
-        "origin_4": 0
+         "origin_1": -2864476,
+        "origin_2": -807440,
+        "origin_3": -802157,
+        "origin_4": -792346
     }
     
     try:
