641910d3016e9d112f17af4e7c487841db75e41b
tzf230201 <tzf230201@gmail.com>
2025-07-18 00:38:41 +0700

    new pvt testing

--- pusirobot/ver_d/b3_motion.py
+++ pusirobot/ver_d/b3_motion.py
@@ -1162,7 +1162,7 @@ def pvt_mode_try_pvt_4(cur_joints, tar_joints, travel_time):
     
     
     for pos, vel, tim in pvt_joint_2:
-        if (vel != 0)
+        if (vel != 0):
             pvt_mode_write_read(ID2, pos, vel, tim)
         
     for pos, vel, tim in pvt_joint_3:
