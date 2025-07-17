8ab33684678208de15e7fdd152f872553c5c5adc
tzf230201 <tzf230201@gmail.com>
2025-07-18 00:38:17 +0700

    new pvt testing

--- pusirobot/ver_d/b2_pvt.py
+++ pusirobot/ver_d/b2_pvt.py
@@ -124,9 +124,10 @@ def pvt_mode_write_read(node_id, wr_p, wr_v, wr_t):
 ############################################ function #############################################################
 
 def pvt_mode_init(group_id, pvt_type = PVT_1, pvt_max_point = 400, pvt_3_lower_limit = 40, pvt_3_upper_limit = 80):
-    reset_node()
-    time.sleep(3)
+    # reset_node()
+    
     init_operation_mode(PVT_MODE)
+    time.sleep(3)
     init_change_group_id(group_id)
     # init_single_motor_change_group_id(ID4, 0x06)
     pvt_mode_set_pvt_max_point(pvt_max_point)
