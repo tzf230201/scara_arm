810eb271071c36d6fdc36529d4d6f3af2be30463
tzf230201 <tzf230201@gmail.com>
2025-07-27 07:37:19 +0700

    new pvt testing

--- pusirobot/ver_d/b5_gui.py
+++ pusirobot/ver_d/b5_gui.py
@@ -402,8 +402,10 @@ def routine():
         if motion_enable and motion_cnt < motion_size:
             root.after(int(routine_interval), routine)
             
+            
             if selection != "servo_only":
                 for i in range(2):
+                    print(pvt_cnt)
                     if pvt_cnt <= max_pvt_index:
                         pos_2, vel_2, tim_2 = pvt_2[pvt_cnt]
                         pos_3, vel_3, tim_3 = pvt_3[pvt_cnt]
