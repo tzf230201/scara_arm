7ffedf1884a6b8397c05071a1f7aa758b8d3aad3
tzf230201 <tzf230201@gmail.com>
2025-07-29 00:45:24 +0700

    new pvt testing

--- pusirobot/ver_d/b3_motion.py
+++ pusirobot/ver_d/b3_motion.py
@@ -944,23 +944,20 @@ def pvt_mode_try_pvt_4(cur_joints, tar_joints, travel_time):
     #     pvt_mode_write_read(ID3, pos, vel, tim)
         
      
-    for pos, vel, tim in pvt4_f:
-        pvt_mode_write_read(ID4, pos, vel, tim)
+    # for pos, vel, tim in pvt4_f:
+    #     pvt_mode_write_read(ID4, pos, vel, tim)
     # for pos, vel, tim in pvt4_b:
     #     pvt_mode_write_read(ID4, pos, vel, tim)
         
     init_single_motor_change_group_id(ID2, group_id)
     init_single_motor_change_group_id(ID3, group_id)
-    init_single_motor_change_group_id(ID4, group_id)
+    # init_single_motor_change_group_id(ID4, group_id)
                 
-    pvt_mode_read_pvt_3_depth()
-    pvt_mode_start_pvt_step(group_id)
-    last_time = time.time()
-    stop_watch = last_time
-    time_out = travel_time / 1000
-
-    return 0
-
+    # pvt_mode_read_pvt_3_depth()
+    # pvt_mode_start_pvt_step(group_id)
+    # last_time = time.time()
+    # stop_watch = last_time
+    # time_out = travel_time / 1000
 
 def stepper_single_motor_pp_mode(id, tar_joint, travel_time,  selection):
     
