575c1f56a288f68e06693a9ed1a9fcad3c9807cd
tzf230201 <tzf230201@gmail.com>
2025-07-29 01:53:47 +0700

    new pvt testing

--- pusirobot/ver_d/b2_pvt.py
+++ pusirobot/ver_d/b2_pvt.py
@@ -130,7 +130,7 @@ def pvt_mode_write_read(node_id, wr_p, wr_v, wr_t):
 ############################################ function #############################################################
 
 def pvt_mode_init(group_id, pvt_type = PVT_1, pvt_max_point = 400, pvt_3_lower_limit = 40, pvt_3_upper_limit = 80):
-    # reset_node()
+    reset_node()
     # stepper_shutdown()
     # time.sleep(1)
     # stepper_init()
