fe8804306c8dd702d32662f9d481c221d53d4001
tzf230201 <tzf230201@gmail.com>
2025-07-29 03:11:06 +0700

    new pvt testing

--- pusirobot/ver_d/b5_gui.py
+++ pusirobot/ver_d/b5_gui.py
@@ -435,7 +435,7 @@ def routine():
                             pvt_cnt = pvt_cnt + 1
                             cur_pvt += 1
                             
-                cur_time = (time.time() - last_time) * 1000
+                # cur_time = (time.time() - last_time) * 1000
                 # print(f"cur time: {cur_time:.2f}, tar_time: {tar_time:.2f}")
                 
                 
