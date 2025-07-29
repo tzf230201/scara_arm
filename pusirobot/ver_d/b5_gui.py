b410830830762e71e484c2db0132401c7caa2c32
tzf230201 <tzf230201@gmail.com>
2025-07-30 05:47:33 +0700

    demo preparation for investor

--- pusirobot/ver_d/b5_gui.py
+++ pusirobot/ver_d/b5_gui.py
@@ -468,11 +468,15 @@ def start_dancing():
         # for node_id in (ID4, ID3, ID2):
         #     init_single_motor_change_group_id(node_id, group_id)
         init_change_group_id(group_id)
-        # time.sleep(1)
+        init_change_group_id(group_id)
+        init_change_group_id(group_id)
+        init_change_group_id(group_id)
+        time.sleep(1)
         
         pvt_mode_start_pvt_step(0x02)
         pvt_mode_start_pvt_step(0x03)
-        # pvt_mode_start_pvt_step(0x04)
+        pvt_mode_start_pvt_step(0x04)
+        pvt_mode_start_pvt_step(0x05)
              
     if selection != "stepper_only":
         entry = motion_data[motion_cnt]
