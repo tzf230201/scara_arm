bfc5cc46c0a7223d1466ed45f22053327d8c9a36
tzf230201 <tzf230201@gmail.com>
2025-07-29 08:55:28 +0700

    new pvt testing

--- pusirobot/ver_d/b3_motion.py
+++ pusirobot/ver_d/b3_motion.py
@@ -268,12 +268,12 @@ def pp_angle(tar_joints, travel_time, selection):
     # print(f"tar max speed = {max_speed_1}, {max_speed_2}, {max_speed_3}, {max_speed_4}")
     
     if selection != "servo_only":  
-        max_speed_2 = stepper_pulses_to_steps(max_speed_2)
-        max_speed_3 = stepper_pulses_to_steps(max_speed_3)
-        max_speed_4 = stepper_pulses_to_steps(max_speed_4)
-        accel_decel_2 = stepper_pulses_to_steps(accel_decel_2)
-        accel_decel_3 = stepper_pulses_to_steps(accel_decel_3)
-        accel_decel_4 = stepper_pulses_to_steps(accel_decel_4)
+        # max_speed_2 = stepper_pulses_to_steps(max_speed_2)
+        # max_speed_3 = stepper_pulses_to_steps(max_speed_3)
+        # max_speed_4 = stepper_pulses_to_steps(max_speed_4)
+        # accel_decel_2 = stepper_pulses_to_steps(accel_decel_2)
+        # accel_decel_3 = stepper_pulses_to_steps(accel_decel_3)
+        # accel_decel_4 = stepper_pulses_to_steps(accel_decel_4)
         pp_mode_set_acceleration(accel_decel_2, accel_decel_3, accel_decel_4)
         pp_mode_set_deceleration(accel_decel_2, accel_decel_3, accel_decel_4)
         pp_mode_set_max_speed(max_speed_2, max_speed_3, max_speed_4)
