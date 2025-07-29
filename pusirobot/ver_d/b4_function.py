b9c1347e5498eaac892486d73b9955c8c7b521c3
tzf230201 <tzf230201@gmail.com>
2025-07-29 23:45:51 +0700

    demo preparation for investor

--- pusirobot/ver_d/b4_function.py
+++ pusirobot/ver_d/b4_function.py
@@ -38,7 +38,9 @@ def wake_up():
             
             time.sleep(0.2)
             servo_brake_off()
-            print(f"brake off")
+            print(f"turned off brake")
+        else:
+            print(f"brake already off")
             
     is_wake_up = True
     
