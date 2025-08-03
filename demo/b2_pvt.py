0f4cdec2a3d96bc8f7b61164378978e3a6d73ad7
tzf230201 <tzf230201@gmail.com>
2025-08-04 01:13:45 +0700

    demo preparation for investor

--- demo/b2_pvt.py
+++ demo/b2_pvt.py
@@ -130,7 +130,7 @@ def pvt_mode_write_read(node_id, wr_p, wr_v, wr_t):
 ############################################ function #############################################################
 
 def pvt_mode_init(group_id, pvt_type = PVT_3, pvt_max_point = 400, pvt_3_lower_limit = 40, pvt_3_upper_limit = 80):
-    # reset_node()
+    reset_node()
     stepper_shutdown()
     stepper_init()
     time.sleep(1)
