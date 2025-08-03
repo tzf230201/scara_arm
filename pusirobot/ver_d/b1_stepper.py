42598d19dbff89eb7b3fec035f0ef54473436859
tzf230201 <tzf230201@gmail.com>
2025-08-03 17:20:35 +0700

    demo preparation for investor

--- pusirobot/ver_d/b1_stepper.py
+++ pusirobot/ver_d/b1_stepper.py
@@ -349,7 +349,7 @@ def stepper_shutdown():
     init_motor_enable(0)  
     init_torque_ring_enable(0)  
     init_set_max_current(0)
-    reset_node()
+    # reset_node()
     
 def stepper_enable_heartbeat():
     for id in [ID2, ID3, ID4]:
