b10e4841e162907d5bd64bca100b9f3baae66b05
tzf230201 <tzf230201@gmail.com>
2025-08-06 14:54:27 +0700

    demo preparation for investor

--- pusirobot/ver_d/b3_motion.py
+++ pusirobot/ver_d/b3_motion.py
@@ -321,6 +321,8 @@ def pp_angle(tar_joints, travel_time, selection):
         accel_decel_2 = stepper_pulses_to_steps(accel_decel_2)
         accel_decel_3 = stepper_pulses_to_steps(accel_decel_3)
         accel_decel_4 = stepper_pulses_to_steps(accel_decel_4)
+        
+        max_speed_2, max_speed_3, max_speed_4 = 10000
         pp_mode_set_acceleration(accel_decel_2, accel_decel_3, accel_decel_4)
         pp_mode_set_deceleration(accel_decel_2, accel_decel_3, accel_decel_4)
         pp_mode_set_max_speed(max_speed_2, max_speed_3, max_speed_4)
