ebbe869084df647afe3ac7e7a7d44d00a5eeb372
tzf230201 <tzf230201@gmail.com>
2025-08-25 19:38:47 +0700

    setup new motor (building uirobot stepper functions)

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ if [ "$(whoami)" != "peter" ]; then
     git status
     git add .
     git status
-    git commit -m "setup new motor (UI robot stepper & DMKE Servo)"
+    git commit -m "setup new motor (building uirobot stepper functions)"
     git push origin main --force
 else
     echo "username 'peter', can't run this script."
