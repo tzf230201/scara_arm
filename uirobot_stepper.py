d076c1ce37ccd89e48920f6c8a14906770400d54
tzf230201 <tzf230201@gmail.com>
2025-08-25 19:25:00 +0700

    setup new motor (UI robot stepper & DMKE Servo)

--- uirobot_stepper.py
+++ uirobot_stepper.py
@@ -74,5 +74,22 @@ def stepper_get_bitrate(node_id):
         return resp["data"][2]   # 2 = 500Kbps
     return None
 
-status = stepper_get_bitrate(6)  # Contoh: Get status motor driver untuk node ID 6
-print(f"Status motor driver untuk node ID 6: {status}")
+BITRATE_MAP = {
+    0: "1000Kbps",
+    1: "800Kbps",
+    2: "500Kbps",
+    3: "250Kbps",
+    4: "125Kbps"
+}
+
+br = stepper_get_bitrate(6)
+if br is not None:
+    print(f"Bitrate Stepper 6: {BITRATE_MAP.get(br, 'Unknown')}")
+else:
+    print("Gagal baca bitrate")
+    
+mo = stepper_get_mo(6)
+if mo is not None:
+    print(f"Status Motor Stepper 6: {'ON' if mo == 1 else 'OFF'}")
+else:
+    print("Gagal baca status motor")
