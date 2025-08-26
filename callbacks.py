81816751a60a024975ff3072c76dae54d06341e3
tzf230201 <tzf230201@gmail.com>
2025-08-26 16:31:33 +0700

    setup new motor (building uirobot stepper functions)

--- callbacks.py
+++ callbacks.py
@@ -2,16 +2,25 @@
 from time import time
 from stepper import *
 
-# def stepper_pulse_to_deg(pulse)
+MICROSTEP = 16
+STEPS_PER_REV = 200 * MICROSTEP # misal 3200 pulse/rev
+DEG_PER_PULSE = 360.0 / STEPS_PER_REV
+
+def stepper_pulse_to_deg(pulse):
+    return pulse * DEG_PER_PULSE
 
 def wake_up(msg, state):
     print(f"[cb] Wake up: motor={msg.get('motor')}")  # Wake up/enable/prepare motors
-    stepper_set_mo(1)
+    stepper_set_mo(6,1)
+    stepper_set_mo(7,1)
+    stepper_set_mo(8,1)
     state['motor_on'] = True
 
 def shutdown(msg, state):
     print(f"[cb] Shutdown: motor={msg.get('motor')}")
-    stepper_set_mo(0)
+    stepper_set_mo(6,0)
+    stepper_set_mo(7,0)
+    stepper_set_mo(8,0)
     state['motor_on'] = False
 
 def pp_joint(msg, state):
@@ -61,14 +70,17 @@ def stop(msg, state):
     stepper_stop_motion(6)
     stepper_stop_motion(7)
     stepper_stop_motion(8)
-    state['running'] = False
+    # state['running'] = False
 
 def read_encoder(msg, state):
     pa2 = stepper_get_pa(6)
     pa3 = stepper_get_pa(7)
     pa4 = stepper_get_pa(8)
 
-    print(f"[cb] Read Position: pa2={pa2} pa3={pa3} pa4={pa4}")
+    pa2_deg = stepper_pulse_to_deg(pa2)
+    pa3_deg = stepper_pulse_to_deg(pa3)
+    pa4_deg = stepper_pulse_to_deg(pa4)
+    print(f"[cb] pa = {pa2_deg}, {pa3_deg}, {pa4_deg} degree")
     # TODO: Implement reading encoder value
 
 def set_origin(msg, state):
