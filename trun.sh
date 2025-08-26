2cf7a05e7b72993ac4733096fa9f47724832268c
tzf230201 <tzf230201@gmail.com>
2025-08-26 21:08:12 +0700

    setup new motor (building uirobot stepper functions)

--- trun.sh
+++ trun.sh
@@ -1,6 +1,6 @@
 ./get.sh
 if [ "$(whoami)" == "peter" ]; then
-    python3 stepper_uirobot/b_ver/main.py
+    python3 stepper_uirobot/b_ver/tester.py
 else
     echo "you are not 'peter', can't run"
 fi
