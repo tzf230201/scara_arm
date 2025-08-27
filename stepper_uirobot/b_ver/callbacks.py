1dde5f028a90a5c024038e85a69c961cab494e92
tzf230201 <tzf230201@gmail.com>
2025-08-27 09:25:40 +0700

    setup new motor (building uirobot stepper functions)

--- stepper_uirobot/b_ver/callbacks.py
+++ stepper_uirobot/b_ver/callbacks.py
@@ -14,27 +14,33 @@ def shutdown(msg, state):
     state['motor_on'] = False
 
 def pp_joint(msg, state):
-    joints = msg.get("joints", [0, 0, 0])
+    # Ambil array lengkap [joint1, joint2, joint3, joint4]
+    joints_full = msg.get("joints", [0, 0, 0, 0])
     t_ms = msg.get("time", 1000)
+    # Hanya gunakan joint2, joint3, joint4
+    # joint2 = joints_full[1], joint3 = joints_full[2], joint4 = joints_full[3]
+    joints = joints_full[1:4]
     node_ids = [6, 7, 8]
+
     pulses = [stepper_deg_to_pulse(j) for j in joints]
     pa_now = [stepper_get_pa(n) for n in node_ids]
     delta = [abs(p - p_now) for p, p_now in zip(pulses, pa_now)]
     t = max(t_ms / 1000.0, 1e-3)
-    # hitung v_max dan acc triangle
-    v_max = max(2 * d / t for d in delta)   # pulses/sec (ambil max dari semua joint)
+
+    v_max = max(2 * d / t for d in delta)
     acc = max(4 * d / (t ** 2) for d in delta)
     v_max = int(max(v_max, 1))
     acc = int(max(acc, 1))
+
     print(f"PP Joint (TRIANGLE): pulses={pulses}, speed={v_max}, acc={acc}, time={t_ms}ms")
     for node_id, pos in zip(node_ids, pulses):
         stepper_set_ac(node_id, acc)
         stepper_set_dc(node_id, acc)
         stepper_set_pa(node_id, pos)
         stepper_set_sp(node_id, v_max)
-        
     for node_id in node_ids:
         stepper_begin_motion(node_id)
+
 
 
 
@@ -78,7 +84,7 @@ def stop(msg, state):
 
 def read_encoder(msg, state):
     pa2, pa3, pa4 = stepper_get_all_enc()
-    print(f"[cb] Read Position: pa2={pa2} pa3={pa3} pa4={pa4}")
+    print(f"[cb] enc: pa2={pa2} pa3={pa3} pa4={pa4}")
     # TODO: Implement reading encoder value
 
 def set_origin(msg, state):
