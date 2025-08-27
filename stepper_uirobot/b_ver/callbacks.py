c3f41d1b5ce62f5cd8502b993ff9793aeb4d7a4c
tzf230201 <tzf230201@gmail.com>
2025-08-27 10:16:54 +0700

    setup new motor (building uirobot stepper functions)

--- stepper_uirobot/b_ver/callbacks.py
+++ stepper_uirobot/b_ver/callbacks.py
@@ -41,7 +41,7 @@ def pvt_coor(msg, state):
 def read_position(msg, state):
     # print(f"[cb] Read Position: motor={msg.get('motor')}")
     pa2_deg, pa3_deg, pa4_deg = stepper_get_all_angle()
-    x,y,z,yaw = forward_kinematics(0,[pa2_deg, pa3_deg, pa4_deg])
+    x,y,z,yaw = forward_kinematics([0,pa2_deg, pa3_deg, pa4_deg])
     print_yellow(f"[cb] pa2={pa2_deg:.2f}, pa3={pa3_deg:.2f}, pa4={pa4_deg:.2f} degree")
     print_orange(f"[cb] x={x:.2f}, y={y:.2f}, z={z:.2f}, yaw={yaw:.2f} degree")
 
