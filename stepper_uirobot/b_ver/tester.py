abf4abd2a9532d66b9517a6c86fe03d5d13f72c5
tzf230201 <tzf230201@gmail.com>
2025-08-27 09:34:51 +0700

    setup new motor (building uirobot stepper functions)

--- stepper_uirobot/b_ver/tester.py
+++ stepper_uirobot/b_ver/tester.py
@@ -84,7 +84,9 @@ import time
 #     print("Tidak ada error (atau gagal baca error report).")
 
 # stepper_set_micro_stepping_resolution(6, 16)
-print("Micro-stepping:", stepper_get_micro_stepping_resolution(6))
+# print("Micro-stepping:", stepper_get_micro_stepping_resolution(6))
+ret = stepper_get_ptp_finish_notification(6)
+print(f"PTP Finish Notification (motor 6): {ret}")
 
 # # print("Set current:", stepper_set_working_current(6, 2.5))
 # print("Working current (A):", stepper_get_working_current(6))
