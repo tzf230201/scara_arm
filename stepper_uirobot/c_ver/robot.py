d0b85363947238fa47b2a1e589a5d1e90e78ff94
tzf230201 <tzf230201@gmail.com>
2025-08-29 11:04:44 +0700

    setup new motor (building uirobot stepper functions)

--- stepper_uirobot/c_ver/robot.py
+++ stepper_uirobot/c_ver/robot.py
@@ -3,20 +3,20 @@ from servo import *
 from stepper import *
 import time
 
-def robot_init():
-    print(f"robot_init()")
-def robot_wake_up():
-    print(f"robot_wake_up()")
-def robot_shutdown():
-    print(f"robot_shutdown()")
-def robot_dancing():
-    print(f"robot_dancing()")
-def robot_get_enc():
-    print(f"robot_get_enc()")
-def robot_get_angle():
-    print(f"robot_get_angle()")
-def robot_set_origin():
-    print(f"robot_set_origin()")
+def robot_init(selection):
+    print(f"robot_init({selection})")
+def robot_wake_up(selection):
+    print(f"robot_wake_up({selection})")
+def robot_shutdown(selection):
+    print(f"robot_shutdown({selection})")
+def robot_dancing(selection):
+    print(f"robot_dancing({selection})")
+def robot_get_enc(selection):
+    print(f"robot_get_enc({selection})")
+def robot_get_angle(selection):
+    print(f"robot_get_angle({selection})")
+def robot_set_origin(selection):
+    print(f"robot_set_origin({selection})")
 
 
 def robot_pp_angle(tar_joints, travel_time, selection):
