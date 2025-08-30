f30b6f6b82f7472001d83e365bb72fdac379b0c3
tzf230201 <tzf230201@gmail.com>
2025-08-30 21:03:30 +0700

    setup new motor (building uirobot stepper functions)

--- stepper_uirobot/c_ver/servo.py
+++ stepper_uirobot/c_ver/servo.py
@@ -132,7 +132,7 @@ def servo_get_bitrate():
     bitrate = req_sdo(ID1, OD_SERVO_CANOPEN_NETWORK_CONFIGURATION, 0x00)
     print(f"bitrate = {bitrate}")
 
-servo_set_bitrate(0x1B00)
+servo_set_bitrate(0x1800)
 servo_get_bitrate()
 
 def servo_accel_decel_calc(d_total, t_travel_ms):
