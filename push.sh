df6a44d1e246ce7e18d18bee5d69cfdf2eb2179c
tzf230201 <tzf230201@gmail.com>
2025-09-05 02:01:44 +0700

    ready for first demo

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ if [ "$(whoami)" != "peter" ]; then
     git status
     git add .
     git status
-    git commit -m "setup new motor (building uirobot stepper functions)"
+    git commit -m "ready for first demo"
     git push origin main --force
 else
     echo "username 'peter', can't run this script."
