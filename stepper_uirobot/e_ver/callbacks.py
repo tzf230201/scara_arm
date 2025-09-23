a798df1e5504d2e16a4c9fa7ac057b890ae17e26
tzf230201 <tzf230201@gmail.com>
2025-09-23 20:15:25 +0900

    develop_gcode_method

--- stepper_uirobot/e_ver/callbacks.py
+++ stepper_uirobot/e_ver/callbacks.py
@@ -120,14 +120,16 @@ def set_origin(msg, state):
 def out2_active(msg, state):
     print(f"out2 active")
     servo_get_output_pin_configuration(2)
+    servo_get_digital_output_state()
     servo_set_digital_output_state(2, 1)
-    # servo_get_digital_output_state()
+    
 
 def out2_nonactive(msg, state):
     print(f"out2 non-active")
     servo_get_output_pin_configuration(2)
+    servo_get_digital_output_state()
     servo_set_digital_output_state(2, 0)
-    # servo_get_digital_output_state()
+    
     
 
 # === Mapping ===
