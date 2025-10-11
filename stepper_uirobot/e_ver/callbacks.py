c93efeabafd5ad301f5ddae6c83d0248762aa414
tzf230201 <tzf230201@gmail.com>
2025-10-11 14:48:41 +0900

    impement request-response feature

--- stepper_uirobot/e_ver/callbacks.py
+++ stepper_uirobot/e_ver/callbacks.py
@@ -112,10 +112,10 @@ def dancing(msg, state):
     # misal: robot_load_pvt_csv(csv_path)
     # atau robot_prepare_motion(csv_path)
     # lalu mulai eksekusi seperti biasa:
-    robot_start_pvt_dancing()
+    # robot_start_pvt_dancing()
 
     # update state
-    state['routine'] = True
+    # state['routine'] = True
 
     
 def routine(state):
