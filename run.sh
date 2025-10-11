d7d95a212bbb797983b55df19e9338305c382331
tzf230201 <tzf230201@gmail.com>
2025-10-11 16:53:27 +0900

    impement request-response feature

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./get.sh
 if [ "$(whoami)" == "peter" ]; then
-    python3 stepper_uirobot/f_ver/main.py
+    python3 stepper_uirobot/e_ver/main.py
 else
     echo "you are not 'peter', can't run"
 fi
