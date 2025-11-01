4c80a52d652bed8924e0e44896649df3dd9f7cbb
tzf230201 <tzf230201@gmail.com>
2025-11-01 15:35:41 +0900

    impement request-response feature

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ if [ "$(whoami)" != "peter" ]; then
     git status
     git add .
     git status
-    git commit -m "delete unncessary files"
+    git commit -m "impement request-response feature"
     git push origin main --force
 else
     echo "username 'peter', can't run this script."
