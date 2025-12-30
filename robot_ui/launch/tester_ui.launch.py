f008033df1a1568531c2ee553d990fa4f61252d6
tzf230201 <teukuzikrifatahillah@gmail.com>
2025-12-30 21:35:44 +0900

    moving to ROS2 Jazzy

--- robot_ui/launch/tester_ui.launch.py
+++ robot_ui/launch/tester_ui.launch.py
@@ -16,15 +16,15 @@ def generate_launch_description() -> LaunchDescription:
             ),
             SetEnvironmentVariable(name='TESTER_UI_TOPIC', value=topic),
             Node(
-                package='robot_controller',
-                executable='controller',
-                name='robot_controller',
-                output='screen',
-            ),
-            Node(
                 package='robot_ui',
                 executable='tester_ui',
                 name='tester_ui',
+                output='screen',
+            ),
+            Node(
+                package='robot_controller',
+                executable='controller',
+                name='robot_controller',
                 output='screen',
             ),
         ]
